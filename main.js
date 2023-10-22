91b97494e450207bbf017488bfcf477a9180b0a8
SoyECS <148662196+SoyECS@users.noreply.github.com>
2023-10-21 20:44:03 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ enviar.addEventListener("click", function()
 {
     if (formulario.username.value && formulario.password.value) 
     {
-        window.location.replace = "https://SoyECS.github.io/body.html";
+        window.location.replace = "etecsa.cu";
     } 
     else 
     {
